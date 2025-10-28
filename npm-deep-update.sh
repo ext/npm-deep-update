c026a7697bf079e1dbd93ff040970fb80c1e159c
David Sveningsson <ext@sidvind.com>
2025-10-28 23:46:48 +0100

    fix: ignore aliased package names

--- npm-deep-update.sh
+++ npm-deep-update.sh
@@ -3,12 +3,19 @@
 set -e
 
 for dep in $(test -e cache.txt && cat cache.txt || npm outdated --all --json | jq -r 'keys[]' | tr -d "\r" | tee cache.txt); do
+  if [[ "${dep}" =~ ":" ]]; then
+    echo "Ignoring ${dep} (updating aliased package not supported)"
+    sed -i '1d' cache.txt
+    continue
+  fi
+
   echo "$dep";
   npm update --no-audit --no-fund --no-update-notifier "$dep"
   if git status --porcelain package-lock.json | grep -q ' M '; then
     git add package-lock.json
     git commit --no-verify -m "chore(deps): update $dep"
   fi
+
   sed -i '1d' cache.txt
 done
 
